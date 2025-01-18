de6c44d7dcefeff4f0591adeacc2d19133636e3d
Wassim Fu <2503102900@qq.com>
2025-01-18 10:39:18 +0800

    第一阶段cmake和gcc学习，删除多余文件节省空间

--- 2_cmake_lib/build/CMakeFiles/hello.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "D:/Documents/c_code_files/cmake_gcc_study/2_cmake_lib/lib/libhello.a"
-)
--- 5_cmake_embed/build/sum/CMakeFiles/sum.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "D:/Documents/c_code_files/cmake_gcc_study/5_cmake_embed/lib/libsum.a"
-)
